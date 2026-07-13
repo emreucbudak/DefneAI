3d375d1752d4fe06f4b6655212d66b577f4e9331
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-07-13 18:59:41 +0300

    Add dynamic model registration

--- DefneAI.Application/InitializerService/IModelInitializerService.cs
+++ DefneAI.Application/InitializerService/IModelInitializerService.cs
@@ -1,7 +1,10 @@
-﻿namespace DefneAI.Application.InitializerService
+﻿using Microsoft.SemanticKernel.Agents;
+
+namespace DefneAI.Application.InitializerService
 {
     public interface IModelInitializerService
     {
         Task<string> InitializeModelAsync();
+        Task<IList<ChatCompletionAgent>> GetChatCompletionAgentsAsync();
     }
 }
--- DefneAI.Infrastructure/InitializerService/ModelInitializerService.cs
+++ DefneAI.Infrastructure/InitializerService/ModelInitializerService.cs
@@ -1,10 +1,11 @@
 ﻿using DefneAI.Application.InitializerService;
 using DefneAI.Application.Repository;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
 
 namespace DefneAI.Infrastructure.InitializerService
 {
-    public class ModelInitializerService(IModelRepository repo,IKernelBuilder kernel) : IModelInitializerService
+    public class ModelInitializerService(IModelRepository repo, IKernelBuilder kernel) : IModelInitializerService
     {
         public async Task<string> InitializeModelAsync()
         {
@@ -14,15 +15,46 @@ namespace DefneAI.Infrastructure.InitializerService
                 foreach (var model in models)
                 {
                     Uri end = new Uri(model.Endpoint);
-                    kernel.AddOpenAIChatCompletion(modelId: model.ModelName, apiKey: model.ApiKey, endpoint: end, serviceId: model.ServiceId);
+                    kernel.AddOpenAIChatCompletion(modelId: model.ModelId, apiKey: model.ApiKey, endpoint: end, serviceId: model.ServiceId);
                 }
                 return "Modeller çalışmaya hazır.";
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 return ex.Message;
-            } 
+            }
         }
-            
+        public async Task<IList<ChatCompletionAgent>> GetChatCompletionAgentsAsync()
+        {
+            var models = await repo.GetAllModelProviders();
+            var modelAgents = new List<ChatCompletionAgent>();
+            Kernel kern = kernel.Build();
+            PromptExecutionSettings prompt = new PromptExecutionSettings()
+            {
+                FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(), 
+            };
+            try
+            {
+                foreach(var model in models)
+                {
+                    prompt.ServiceId = model.ServiceId;
+                    var modelAgent = new ChatCompletionAgent()
+                    {
+                        Name = model.ModelName,
+                        Description = model.ModelDescription,
+                        Kernel = kern,
+                        Arguments = new KernelArguments(prompt),
+                        Instructions = model.ModelInstructions
+                    };
+                    modelAgents.Add(modelAgent);
+                }
+                return modelAgents;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
+}
 
